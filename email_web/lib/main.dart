fb12559346f198077690166d8a9fbe9cc35bb5b4
vincecfl <vincecfl@gmail.com>
2021-03-15 00:34:10 +0800

    add email web

---
+++ email_web/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:email_web/screens/main/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Email UI ',
+      theme: ThemeData(),
+      home: MainScreen(),
+    );
+  }
+}
